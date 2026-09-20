c4259568e3777e0f04cceed1606ccf5e266ff7c0
Sviatozar Ivasiv <sviatozar007@gamil.com>
2026-09-20 20:54:22 +0300

    lab01: solution CrossApp, Cli, вибір домену

---
+++ src/Cli/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+Console.WriteLine("CrossApp – практикум з крос-платформного програмування");
+Console.WriteLine("Студент: Івасів Святозар, ФЕІ-37");
+Console.WriteLine(new string('-', 52));
+Console.WriteLine($"Архітектура процесу  : {RuntimeInformation.ProcessArchitecture}");
+Console.WriteLine($"Версія .NET (CLR)    : {Environment.Version}");
+Console.WriteLine($"Runtime              : {RuntimeInformation.FrameworkDescription}");
+Console.WriteLine($"Каталог застосунку   : {AppContext.BaseDirectory}");
+Console.WriteLine($"Поточний каталог     : {Environment.CurrentDirectory}");
+Console.WriteLine(new string('-', 52));
+Console.WriteLine("Предметна область: Склад (товари, партії, залишки, переміщення)");
